36b452334971cea78ebe27eb069be1b0bd2464b2
Galen Church <galenechurch@gmail.com>
2018-06-08 10:49:26 -0400

    refactored LED methods

--- led.go
+++ led.go
@@ -4,23 +4,36 @@ import (
 	"fmt"
 )
 
-type led struct {
-	base, name string
+type LED struct {
+	Sys   sysfs_led
+	Value string //"0" or "1"
 }
 
+//LED Globals
 var (
-	Usr0 = ledBase + "/beaglebone:green:usr0"
-	Usr1 = ledBase + "/beaglebone:green:usr1"
-	Usr2 = ledBase + "/beaglebone:green:usr2"
-	Usr3 = ledBase + "/beaglebone:green:usr3"
+	Usr0 = LED{Sys: sysfs_led{SysfsLOC: "/beaglebone:green:usr0", Name: "usr0"}}
+	Usr1 = LED{Sys: sysfs_led{SysfsLOC: "/beaglebone:green:usr1", Name: "usr1"}}
+	Usr2 = LED{Sys: sysfs_led{SysfsLOC: "/beaglebone:green:usr2", Name: "usr2"}}
+	Usr3 = LED{Sys: sysfs_led{SysfsLOC: "/beaglebone:green:usr3", Name: "usr3"}}
 )
 
-func LedOff(l string) {
+//GetLEDSysfs - returns complete abs path to gpio dir
+//
+func (l *LED) GetLEDSysfs() string {
+	return ledBase + l.Sys.SysfsLOC
+}
+
+//LedOff - turns LED off
+//
+func (l *LED) LedOff() {
 	fmt.Printf("LED %s off\n", l)
-	WriteAndClose(l+"/brightness", "0")
+	WriteAndClose(l.GetLEDSysfs()+"/brightness", "0")
 
 }
-func LedOn(l string) {
+
+//LedOn - turns LED on
+//
+func (l *LED) LedOn() {
 	fmt.Printf("LED %s on\n", l)
-	WriteAndClose(l+"/brightness", "1")
+	WriteAndClose(l.GetLEDSysfs()+"/brightness", "1")
 }
--- sysfs.go
+++ sysfs.go
@@ -1,5 +1,11 @@
 package gobone
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 const gpioBase string = "/sys/class/gpio"
 
 type sysfs_gpio struct {
@@ -10,3 +16,49 @@ type sysfs_gpio struct {
 }
 
 const ledBase string = "/sys/class/leds"
+
+type sysfs_led struct {
+	Name     string
+	SysfsLOC string
+}
+
+//WriteAndClose - Writes v to file passed as fi
+//TODO add checks
+func WriteAndClose(fi string, v string) {
+	f, err := os.OpenFile(fi, os.O_WRONLY|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(v)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Write to %s\n", fi)
+}
+
+//ReadByteAndClose - Returns []byte read from file fi
+//
+func ReadByteAndClose(fi string) []byte {
+
+	var size int
+	v := make([]byte, 1)
+	f, err := os.OpenFile(fi, os.O_RDONLY, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	size, err = f.Read(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Read %v from %s\nWas %d bytes in len", v, fi, size)
+	return v
+}
